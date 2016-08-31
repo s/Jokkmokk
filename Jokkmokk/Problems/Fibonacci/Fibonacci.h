ee1273344f5f42f75f5a854964e00f2af802a329
Said <saidozcn@gmail.com>
2016-08-31 13:41:38 +0300

    add Fibonacci files

---
+++ Jokkmokk/Problems/Fibonacci/Fibonacci.h
@@ -0,0 +1,13 @@
+//
+//  Fibonacci.h
+//  Jokkmokk
+//
+//  Created by Muhammed Said Özcan on 31/08/16.
+//  Copyright © 2016 Muhammed Said Özcan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fibonacci : NSObject
+
+@end
---
+++ Jokkmokk/Problems/Fibonacci/Fibonacci.m
@@ -0,0 +1,13 @@
+//
+//  Fibonacci.m
+//  Jokkmokk
+//
+//  Created by Muhammed Said Özcan on 31/08/16.
+//  Copyright © 2016 Muhammed Said Özcan. All rights reserved.
+//
+
+#import "Fibonacci.h"
+
+@implementation Fibonacci
+
+@end
