f9320fb2c1e8f15ebe2e7a5c3a53483245b962a3
Said <saidozcn@gmail.com>
2016-09-02 11:13:08 +0300

    add initial Substring files

---
+++ Jokkmokk/Problems/Substringer/Substringer.h
@@ -0,0 +1,13 @@
+//
+//  Substringer.h
+//  Jokkmokk
+//
+//  Created by Muhammed Said Özcan on 02/09/16.
+//  Copyright © 2016 Muhammed Said Özcan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Substringer : NSObject
+
+@end
---
+++ Jokkmokk/Problems/Substringer/Substringer.m
@@ -0,0 +1,13 @@
+//
+//  Substringer.m
+//  Jokkmokk
+//
+//  Created by Muhammed Said Özcan on 02/09/16.
+//  Copyright © 2016 Muhammed Said Özcan. All rights reserved.
+//
+
+#import "Substringer.h"
+
+@implementation Substringer
+
+@end
